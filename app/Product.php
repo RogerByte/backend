4f558d1fd86eb4f8c0decabf1468ec569f4f0194
Roger Maldonado <marioroger11@gmail.com>
2017-05-20 23:51:59 -0500

    Relación de model

--- app/Product.php
+++ app/Product.php
@@ -2,9 +2,42 @@
 
 namespace App;
 
+use App\Seller;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+	const PRODUCT_AVAILABLE = 'disponible';
+	const PRODUCT_UNAVALIABLE = 'no disponible';
+
+	protected $fillable = [
+		'name',
+		'description',
+		'quantity',
+		'status',
+		'image',
+		'seller_id'
+	];
+
+	public function available()
+	{
+		return $this->status == Product::PRODUCT_AVAILABLE;
+	}
+
+	public function seller()
+	{
+		return $this->belongsTo(Seller::class);
+	}
+
+	public function transactions()
+	{
+		return $this->hasMany(Transaction::class);
+	}
+
+	public function categories()
+	{
+		return $this->belongsToMany(Category::class);
+	}
 }
