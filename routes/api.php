6177d3633c368170aff4e6fe789cd36e2b654726
Roger Maldonado <marioroger11@gmail.com>
2017-05-28 15:34:45 -0500

    Operaciones complejas para seller

--- routes/api.php
+++ routes/api.php
@@ -49,6 +49,10 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
  * Sellers
  */
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index','show']]);
+Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' => ['create','show','edit']]);
 
 /**
  * Users
